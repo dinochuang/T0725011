bfd03088ac2de829a30e82bcec6c620868e99607
dino <dino6535@hotmail.com>
2016-07-26 09:27:48 +0800

    對話框

--- app/src/main/java/com/example/student/t072501/MainActivity.java
+++ app/src/main/java/com/example/student/t072501/MainActivity.java
@@ -111,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
         builder.setMultiChoiceItems(sweets, b, new DialogInterface.OnMultiChoiceClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                b[which] = isChecked;
+               // b[which] = isChecked;
             }
         });
 
